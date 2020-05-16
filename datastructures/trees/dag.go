6774be890b7da4175e7280f2162dd2c71c8f2708
timolinn <timothyonyiuke@gmail.com>
2020-05-16 17:54:47 +0100

    init code problems file

--- datastructures/trees/dag.go
+++ datastructures/trees/dag.go
@@ -5,6 +5,12 @@ package trees
 // it is widely used in the blockchain space
 // where they are used in-place of maybe linked-lists
 // for distributed ledger data structures
+// In order to support the ability to push
+// and pull changesets between multiple instances of the same repository,
+// we need a specially designed structure for representing multiple versions of things.
+// The structure we use is called a Directed Acyclic Graph (DAG),
+// a design which is more expressive than a purely linear model.
+// The history of everything in the repository is modeled as a DAG. This sounds like Git.
 type DAG struct {
 	nodes map[int]*DAGNode
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0
+	github.com/hashicorp/terraform v0.12.25
 	github.com/teivah/bitvector v0.0.0-20190716215529-286a776f46ca
 )
